6ebc47b716d42dc77558217159ccbafc40b32970
Mark <73572656+markee2301@users.noreply.github.com>
2023-12-12 08:36:38 +0000

    Fixed api handling error.

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ from langchain.vectorstores import FAISS
 from langchain.chat_models import ChatOpenAI
 from langchain.memory import ConversationBufferMemory
 from langchain.chains import ConversationalRetrievalChain
-from styles import css, bot_template, user_template, user_question
+from styles import css, bot_template, user_template
 
 def get_pdf_text(pdf_docs):
     text = ""
